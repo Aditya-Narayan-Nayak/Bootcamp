0b00980f0f03d957c84e0701967a737b2a849938
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2021-11-13 00:50:29 +0530

    Write a program to remove the last N item in the list

---
+++ python/Recursion/Remove_the_Last_Items.py
@@ -0,0 +1,5 @@
+programming_languages = ['Python', 'C', 'Java', 'Ruby', 'C++', 'CSS', 'HTML', 'Bash', 'Perl', 'R', 'Swift', 'SQL', 'PHP', 'JavaScript']
+n = int(input())
+for i in range(n):
+    programming_languages.pop()
+print(programming_languages)

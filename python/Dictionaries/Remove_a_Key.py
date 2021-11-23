9bb8a87b5784008a52c7dbbdbaff15f2e61c1576
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2021-11-23 12:20:57 +0530

    for this problem, the prefilled code will contain a dictionary. Write a program to remove a key.

---
+++ python/Dictionaries/Remove_a_Key.py
@@ -0,0 +1,12 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket",
+    "Deepak": "Boxing"
+}
+
+# Write your code here
+key = input()
+del students_dict[key]
+print(students_dict)

8455ddf21425224b4d15c004116075b133594fc8
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2021-11-23 12:18:28 +0530

    For this problem, the prefilled code will contain a dictionary. Write a program to add a key-value pair to the dictionary

---
+++ python/Dictionaries/Add_a_Key.py
@@ -0,0 +1,10 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket"
+}
+
+key, value = input().split()
+students_dict[key] = value
+print(students_dict)

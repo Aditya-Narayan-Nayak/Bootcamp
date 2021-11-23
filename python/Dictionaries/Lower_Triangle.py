2d6aca2b3756ed7976a769b75ffaea278d1dd29a
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2021-11-23 12:14:05 +0530

    For this problem, the prefilled code will contain an MxN matrix. Write a program to print the lower triangle of the matrix.<br>Lower triangular matrix is a matrix which contain elements below principle diagonal including principle diagonal elements.

---
+++ python/Dictionaries/Lower_Triangle.py
@@ -0,0 +1,23 @@
+def print_lower_triangle(matrix):
+    for i in range(len(matrix)):
+        row_list = matrix[i][:i+1]
+        print(row_list)
+
+def convert_string_to_int(list_a):
+    new_list = []
+    for item in list_a:
+        num = int(item)
+        new_list.append(num)
+    return new_list
+
+
+m, n = input().split()
+m, n = int(m), int(n)
+num_list = []
+
+for i in range(m):
+    list_a = input().split()
+    list_a = convert_string_to_int(list_a)
+    num_list.append(list_a)
+
+print_lower_triangle(num_list)

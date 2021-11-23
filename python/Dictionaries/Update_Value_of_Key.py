24c23e6214c9fd9d89c3a771c162476a73cefe46
Aditya Narayan nayak <72183256+Aditya-Narayan-Nayak@users.noreply.github.com>
2021-11-23 12:19:49 +0530

    For this problem, the prefilled code will contain a dictionary. Write a program to update the value of a given key.

---
+++ python/Dictionaries/Update_Value_of_Key.py
@@ -0,0 +1,12 @@
+students_dict = {
+    "Ram": "Cricket",
+    "Naresh": "Football",
+    "Vani": "Tennis",
+    "Rahim": "Cricket",
+    "Deepak": "Boxing"
+}
+
+# Write your code here
+key, value = input().split()
+students_dict[key] = value
+print(students_dict)
